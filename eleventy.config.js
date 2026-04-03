072ec894fec933ffe57546a38b41d515f587db5c
Desmond Rivet <desmond.rivet@gmail.com>
2026-04-02 20:25:54 -0400

    Next, prev links

--- eleventy.config.js
+++ eleventy.config.js
@@ -52,28 +52,51 @@ export default async function(eleventyConfig) {
     return navNodes;
   });
 
-  eleventyConfig.addFilter('defaultPhotoTitle', (albumNodes, albumByKey, photoKey, parentKey) => {
-    const photoIdx = albumNodes.findIndex(e => e.key === photoKey ) + 1;
+  eleventyConfig.addFilter('photoContext', (albumNodes, albumByKey, photoKey, parentKey) => {
+    const photoIdx = albumNodes.findIndex(e => e.key === photoKey );
+
     const parentNode = albumByKey[parentKey];
-    return `${parentNode.data.title} / ${photoIdx} of ${albumNodes.length}`;
+    const defaultTitle = `${parentNode.data.title} / ${photoIdx + 1} of ${albumNodes.length}`;
+    
+    let prevUrl;
+    if (photoIdx > 0) {
+      const prevNavNode = albumNodes[photoIdx-1];
+      const prevNode = albumByKey[prevNavNode.key];
+      prevUrl = prevNode.url;
+    }
+
+    let nextUrl;
+    if ((photoIdx + 1) < albumNodes.length) {
+      const nextNavNode = albumNodes[photoIdx + 1];
+      const nextNode = albumByKey[nextNavNode.key];
+      nextUrl = nextNode.url;
+    }
+    return {defaultTitle, prevUrl, nextUrl};
   });
 
   eleventyConfig.addFilter("cdump", o => inspect(o));
   
   eleventyConfig.addPassthroughCopy({"static": "."});
 
-  async function makeImage(src, width) {
+  async function imgUrl(src) {
+    const data = await Image("src/"+src, {
+      widths: ["auto"],
+      formats: ["jpeg"],
+    });
+    return data["jpeg"][0].url;
+  }
+  eleventyConfig.addNunjucksAsyncShortcode("imgUrl", imgUrl);
+
+  
+  async function imgShortcode(src, alt, cls, widths) {
     // the src parameter begins with /photo/* which worked when the HTML
     // transform plugin was used, but which doesn't work in this context.
     // We can compensate by tacking on a src/
-    return await Image("src/"+src, {
-      widths: [width],
+    const data = await Image("src/"+src, {
+      widths: [...widths],
       formats: ["jpeg"],
     }); 
-  }
-  
-  async function imgShortcode(src, alt, cls, width) {
-    const data = await makeImage(src, width);
+    
     const allCls = data['jpeg'][0].height > data['jpeg'][0].width ? `${cls} portrait` : cls;
     const attributes = {
       alt,
@@ -86,21 +109,21 @@ export default async function(eleventyConfig) {
   }
 
   async function largeImage(src, alt, cls) {
-    return await imgShortcode(src, alt, cls, 1200);
+    return await imgShortcode(src, alt, cls, [1200]);
   }
 
   async function mediumImage(src, alt, cls) {
-    return await imgShortcode(src, alt, cls, 800);
+    return await imgShortcode(src, alt, cls, [800]);
   }
   
   async function smallImage(src, alt, cls) {
-    return await imgShortcode(src, alt, cls, 480);
+    return await imgShortcode(src, alt, cls, [480]);
   }
   
   eleventyConfig.addNunjucksAsyncShortcode("smallImage", smallImage);
   eleventyConfig.addNunjucksAsyncShortcode("mediumImage", mediumImage);
   eleventyConfig.addNunjucksAsyncShortcode("largeImage", largeImage);
-  
+
   return {
     dir: {
       input: 'src'
